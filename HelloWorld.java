a6751f3e22551895f66ddcd9ade03f41ba95e4f4
darshan050788 <57419372+darshan050788@users.noreply.github.com>
2021-04-06 16:41:38 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,7 +35,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
-    System.out.println("Hiiiii , World);
-    System.out.println("Hiiiii , World - This is my first webhook push");
   }
 }
