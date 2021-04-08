835d724a386ffd4823d14a7d1708026def99c4ad
root <root@ip-172-31-38-7.ec2.internal>
2021-04-08 20:15:31 +0000

    removed msg

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,10 +35,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World - This is addition of code");
-    System.out.println("Hello, World - This is webhooks second  addition of code");
+    System.out.println("Hello, World"); 
   }
 }
