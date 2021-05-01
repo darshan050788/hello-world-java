de22d4776d37433060467286d75310f07df7e0ef
root <root@ip-172-31-21-92.ec2.internal>
2021-05-01 21:36:25 +0000

    msg reduced

--- HelloWorld.java
+++ HelloWorld.java
@@ -37,8 +37,5 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, World"); 
     System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World   ADDDDDDDDDDDDDD");
   }
 }
