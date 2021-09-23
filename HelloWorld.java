d737b98ba7ef7a1819f9d0a1288efabed0927367
root <root@ip-172-31-91-229.ec2.internal>
2021-09-23 21:20:07 +0000

    updated message

--- HelloWorld.java
+++ HelloWorld.java
@@ -43,6 +43,6 @@ public class HelloWorld {
     System.out.println("Hello, World Line 4");
     System.out.println("Hello, World Line 5");
     System.out.println("Hello, World Line 6");
-    System.out.println("Polling SCM - WIll trigger after 1 min");
+    System.out.println("Created Github-Webhooks and using Jenkins");
   }
 }
